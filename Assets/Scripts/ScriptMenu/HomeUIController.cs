ed79cedd7174e1eabd90df0b4cb7f7575b7482e1
Vanh0201 <doanvietanh0201@gmail.com>
2025-05-02 13:33:44 +0700

    update code panel rename

--- Assets/Scripts/ScriptMenu/HomeUIController.cs
+++ Assets/Scripts/ScriptMenu/HomeUIController.cs
@@ -5,12 +5,15 @@ public class HomeUIController : MonoBehaviour
     [SerializeField] private GameObject shopPanel;
     [SerializeField] private ShopTabController shopTabController; 
     [SerializeField] private GameObject settingPanel;
+    // goi mở thanh rename
+    [SerializeField] private GameObject renamePanel;
 
     // gọi nhạc ở đâyđây
     private void Start()
     {
         AudioManager.Instance.PlayMusic(AudioManager.Instance.bgmHome);
         settingPanel.SetActive(false);
+        renamePanel.SetActive(false);
     }
                                     
     public void OpenShop()
@@ -36,5 +39,15 @@ public class HomeUIController : MonoBehaviour
     public void CloseSetting()
     {
         settingPanel.SetActive(false); 
+    }
+
+    // open rename
+    public void OpenRename(){
+        renamePanel.SetActive(true);
+    }
+
+    // close rename
+    public void CloseRename(){
+        renamePanel.SetActive(false);
     }
 }
